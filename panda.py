f19037cf7b29430ae4e2d8cd9293cd0530cdaf26
Luciana-fa <cati23816@gmail.com>
2022-03-23 12:55:29 +0100

    Tables to Dataframes "better" approach

--- panda.py
+++ panda.py
@@ -1,19 +1,47 @@
 import sqlite3
+from tokenize import String
+
 import pandas as pd
 
 con = sqlite3.connect('practicaSI.db')
 cur = con.cursor()
 
 
-def fetch_tables(table, columns):
-    cur.execute("SELECT " + columns + " FROM " + table)
+def fetch_tables(table: str, columns: str, condition):
+    if condition is None:
+        cur.execute("SELECT " + columns + " FROM " + table)
+    else:
+        cur.execute("SELECT " + columns + " FROM " + table + " WHERE " + condition)
     return cur.fetchall()
 
 
-def fetch_tables_condition(table, columns, condition):
-    cur.execute("SELECT " + columns + " FROM " + table + " WHERE " + condition)
-    return cur.fetchall()
+def array_to_string(columns) -> str:
+    if len(columns) == 0:
+        return ValueError
+    else:
+        string_result: str = columns[0]
+        for c in range(1, len(columns)):
+            string_result = string_result + ", " + columns[c]
+        return string_result
 
-legal_content = fetch_tables_condition("legal", "*", "url IS NOT 'missing'")
-no_missing_legal_df = pd.DataFrame(legal_content, columns=['url','cookies', 'aviso', 'proteccion_datos', 'creacion'])
-print(no_missing_legal_df)
+
+def create_dataframe(table: str, columns: str, condition: str):
+    if condition is None:
+        return pd.DataFrame(fetch_tables(table, array_to_string(columns), None), columns=columns)
+    else:
+        return pd.DataFrame(fetch_tables(table, array_to_string(columns), condition), columns=columns)
+
+
+no_missing_legal_df = create_dataframe("legal", ["url", "cookies", "aviso", "proteccion_datos", "creacion"],
+                                       "url IS NOT 'None'")
+no_missing_passwd_df = create_dataframe("users", ["nick", "permisos", "passwd"], "passwd IS NOT 'None'")
+no_missing_telefono_df = create_dataframe("users", ["nick", "permisos", "telefono"], "telefono IS NOT 0")
+no_missing_provincia_df = create_dataframe("users", ["nick", "permisos", "provincia"], "provincia IS NOT 'None'"),
+no_missing_emails_df = create_dataframe("users", ["nick", "permisos", "email_total", "email_phising", "email_click"],
+                                        "email_total > 0")
+"""
+In case of wanting special column name, fetch_tables should be invoked directly
+"""
+no_missing_ips_df = pd.DataFrame(fetch_tables("ips", "*", None), columns=["nick", "ip"])
+no_missing_fechas_df = pd.DataFrame(fetch_tables("fechas", "*", None), columns=["nick", "fecha"])
+print(no_missing_ips_df)
