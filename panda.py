de657809a87dad6659ad986206569ca99ce85db9
Luciana-fa <cati23816@gmail.com>
2022-03-26 18:49:41 +0100

    Ex 4 without graphs

--- panda.py
+++ panda.py
@@ -217,10 +217,13 @@ print("Mayor o igual que 200: ",
 """
 Ejercicio 4
 """
-
+"""
+Usuarios criticos
+"""
 users_df = create_dataframe("users", ["nick", "passwd", "email_click", "email_total", "email_phishing"], None)
 
 usuarios_criticos_df = pd.DataFrame(columns=['nick', 'email_phishing', 'email_click'])
+
 for i in users_df['passwd'].index:
     f = open("resources/smallRockYou.txt", "rt")
     for line in f.readlines():
@@ -242,3 +245,41 @@ for index in usuarios_criticos_df['email_phishing'].index:
 
 usuarios_criticos_df.sort_values(by=['prob_click'], ascending=False, inplace=True)
 print("Los 10 usuarios mas criticos son: \n", usuarios_criticos_df.head(10))
+
+"""
+Politicas de privacidad
+"""
+
+legal_politicas_df = create_dataframe("legal", ["url", "cookies", "aviso", "proteccion_datos", "creacion"], None)
+legal_politicas_df = legal_politicas_df.assign(policies_sum=lambda x: x.aviso + x.proteccion_datos + x.cookies)
+legal_politicas_df.sort_values(by=['policies_sum'], ascending=True, inplace=True)
+print("\nLas 5 webs mas desactualizadas son: \n",legal_politicas_df.head(5))
+
+"""
+IPs
+"""
+ip_count_df = no_missing_ips_df.groupby('nick').count()
+nicks_vuln = usuarios_criticos_df['nick']
+ip_vuln_df = pd.merge(ip_count_df, nicks_vuln, how='inner', on='nick')
+print("\nMedia de IPs de usuarios vulnerables: ", ip_vuln_df['ip'].mean())
+
+all_nicks = missing_users_df['nick']
+no_vuln_nick = pd.concat([all_nicks, nicks_vuln]).drop_duplicates(keep=False)
+ip_no_vuln_df = pd.merge(ip_count_df, no_vuln_nick, how='inner', on='nick')
+print("Media de IPs de usuarios no vulnerables: ", ip_no_vuln_df['ip'].mean())
+
+"""
+WEB
+"""
+all_policies_df = legal_politicas_df[legal_politicas_df['policies_sum'] == 3]
+all_policies_df = all_policies_df.sort_values(by=['creacion'])
+print("\nCumplen las politicas: \n", all_policies_df)
+bad_policies_df = pd.concat([all_policies_df,legal_politicas_df]).drop_duplicates(keep=False)
+bad_policies_df = bad_policies_df.sort_values(by=['creacion'])
+print("No cumplen las politicas: \n",bad_policies_df)
+
+"""
+Pass comprometidas
+"""
+print("\nContrasenas comprometidas: ", nicks_vuln.count())
+print("Contrasenas no comprometidas: ", no_vuln_nick.count())
