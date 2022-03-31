d9a1913ff397da74d8e0f92c703625b41359605f
Lu-all <lucianaallende60@outlook.com>
2022-03-31 23:40:51 +0200

    Basic graphs added

--- html/html_init.py
+++ html/html_init.py
@@ -1,4 +1,7 @@
+import json
+
 from flask import Flask, render_template
+import plotly.graph_objects as go
 
 app = Flask(__name__)
 
@@ -15,27 +18,67 @@ def about_us():
 
 @app.route('/vulnerabilidades')
 def vulnerabilidades():
-    return render_template('vulnerabilidades.html')
+    fig = go.Figure(
+        data=[go.Bar(y=[2, 1, 3])],
+        layout_title_text="Figura"
+    )
+    # fig.show()
+    import plotly
+    a = plotly.utils.PlotlyJSONEncoder
+    graphJSON = json.dumps(fig, cls=a)
+    return render_template('vulnerabilidades.html', graphJSON=graphJSON)
 
 
 @app.route('/login')
 def login():
+    fig = go.Figure(
+        data=[go.Bar(y=[2, 1, 3])],
+        layout_title_text="Figura"
+    )
+    # fig.show()
+    import plotly
+    a = plotly.utils.PlotlyJSONEncoder
+    graphJSON = json.dumps(fig, cls=a)
     return render_template('login.html')
 
 
 @app.route('/usuariosCriticos')
 def usuarios_criticos():
-    return render_template('usuariosCriticos.html')
+    fig = go.Figure(
+        data=[go.Bar(y=[2, 1, 3])],
+        layout_title_text="Figura"
+    )
+    # fig.show()
+    import plotly
+    a = plotly.utils.PlotlyJSONEncoder
+    graphJSON = json.dumps(fig, cls=a)
+    return render_template('usuariosCriticos.html', graphJSON=graphJSON)
 
 
 @app.route('/paginasCriticas')
 def paginas_vulnerables():
-    return render_template('paginasVulnerables.html')
+    fig = go.Figure(
+        data=[go.Bar(y=[2, 1, 3])],
+        layout_title_text="Figura"
+    )
+    # fig.show()
+    import plotly
+    a = plotly.utils.PlotlyJSONEncoder
+    graphJSON = json.dumps(fig, cls=a)
+    return render_template('paginasVulnerables.html', graphJSON=graphJSON)
 
 
 @app.route('/extra')
 def extra():
-    return render_template('extra.html')
+    fig = go.Figure(
+        data=[go.Bar(y=[2, 1, 3])],
+        layout_title_text="Figura"
+    )
+    # fig.show()
+    import plotly
+    a = plotly.utils.PlotlyJSONEncoder
+    graphJSON = json.dumps(fig, cls=a)
+    return render_template('extra.html', graphJSON=graphJSON)
 
 
 if __name__ == '__main__':
