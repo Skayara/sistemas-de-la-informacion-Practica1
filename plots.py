dbe01a5e5ce05841d179ad4959a4ed992a345b8e
0xEileen <i.garciaru.2019@alumnos.urjc.es>
2022-04-28 17:22:28 +0200

    Algorithm complete. Plot axis missing (Regression)

--- plots.py
+++ plots.py
@@ -8,8 +8,17 @@ import pandas as pd
 import plotly
 import plotly.graph_objects as go
 import requests as requests
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn import tree
+import graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import export_graphviz
+from subprocess import call
 
-con = sqlite3.connect('../resources/practicaSI.db', check_same_thread=False)
+con = sqlite3.connect('C:\\Users\\eilee\\Desktop\\pythonProject\\resources\\practicaSI.db', check_same_thread=False)
 cur = con.cursor()
 
 """
@@ -250,54 +259,93 @@ def get_vulnerabilities_point_and_bar(number_of_vuln: int) -> str:
 """
 Machine Learning
 """
+
 """
+Load Data
+"""
+user_JSON = open("resources/json/users_IA_clases.json", "r")
+user_info = json.load(user_JSON)
+
+usuario = []
+emails_phishing_recibidos = []
+emails_phishing_clicados = []
+emails = []
+vulnerable = []
+
+for line in user_info['usuarios']:
+    usuario.append(line['usuario'].replace(".", " "))
+    emails_phishing_recibidos.append(line['emails_phishing_recibidos'])
+    emails_phishing_clicados.append(line['emails_phishing_clicados'])
+    if (line['emails_phishing_recibidos'] != 0):
+        emails.append(line['emails_phishing_clicados'] / line['emails_phishing_recibidos'])
+    else:
+        emails.append(0)
+
+    vulnerable.append(line['vulnerable'])
+
+user_X = pd.DataFrame({'phishing': emails})
+# user_X = pd.DataFrame({'usuario': usuario, 'phishing_recibido': emails_phishing_recibidos, 'phishing_clicado': emails_phishing_clicados})
+user_Y = pd.DataFrame({'vulnerable': vulnerable})
+
+# print(user_X)
+
 # REGRESION LINEAL
-#https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html?highlight=linear%20regression#sklearn.linear_model.LinearRegression
-import numpy as np
-from sklearn.linear_model import LinearRegression
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-# y = 1 * x_0 + 2 * x_1 + 3
-y = np.dot(X, np.array([1, 2])) + 3
-reg = LinearRegression().fit(X, y)
-reg.score(X, y)
 
-reg.coef_
+user_X_train = user_X[:-20]
+user_X_test = user_X[-20:]
 
-reg.intercept_
+user_Y_train = user_Y[:-20]
+user_Y_test = user_Y[-20:]
 
-reg.predict(np.array([[3, 5]]))
+reg = LinearRegression()
+reg.fit(user_X_train, user_Y_train)
+print(reg.coef_)
+user_Y_pred = reg.predict(user_X_test)
+
+print("Mean squared error: %.2f" % mean_squared_error(user_Y_test, user_Y_pred))
 
 # Plot outputs
-plt.scatter(x, y, color="black")
-plt.plot(x, diabetes_y_pred, color="blue", linewidth=3)
+# print("pred:" ,user_X_test)
 
+plt.scatter(user_X_test.values.ravel(), user_Y_test, color="black")
+plt.plot(user_X_test.values.ravel(), user_Y_pred, color="blue", linewidth=3)
 plt.xticks(())
 plt.yticks(())
-
 plt.show()
 
 # DECISION TREE
-#https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html?highlight=decision%20tree#sklearn.tree.DecisionTreeClassifier
-from sklearn.datasets import load_iris
-from sklearn.model_selection import cross_val_score
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
-clf = DecisionTreeClassifier(random_state=0)
-iris = load_iris()
-cross_val_score(clf, iris.data, iris.target, cv=10)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(user_X_train, user_Y_train)
 
-tree.plot_tree(clf)
+# Print plot
+dot_data = tree.export_graphviz(clf, out_file=None)
+graph = graphviz.Source(dot_data)
+graph.render("Usuarios Criticos")
+dot_data = tree.export_graphviz(clf, out_file=None,
+                                feature_names=['email click'],
+                                class_names=['No vulnerable', 'Vulnerable'],
+                                filled=True, rounded=True,
+                                special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render('test.gv', view=True).replace('\\', '/')
 
 # RANDOM FOREST
-#https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html?highlight=random%20forest#sklearn.ensemble.RandomForestClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-X, y = make_classification(n_samples=1000, n_features=4,
-                           n_informative=2, n_redundant=0,
-                           random_state=0, shuffle=False)
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(X, y)
+clf = RandomForestClassifier(max_depth=5, random_state=0, n_estimators=10)
+clf.fit(user_X_train.values.ravel().reshape(-1, 1), user_Y_train.values.ravel())
+print(user_Y_train.values.ravel())
+print(user_X_train.values.ravel().reshape(-1, 1))
+# print(clf.predict([user_X_train.values.ravel()]))
 
-print(clf.predict([[0, 0, 0, 0]]))
-
+for i in range(len(clf.estimators_)):
+    print(i)
+    estimator = clf.estimators_[i]
+    export_graphviz(estimator,
+                    out_file='./treeDir/tree.dot',
+                    feature_names=['email click'],
+                    class_names=['No vulnerable', 'Vulnerable'],
+                    rounded=True, proportion=False,
+                    precision=2, filled=True)
+    call(['dot', '-Tpng', 'C:\\Users\\eilee\\Desktop\\pythonProject\\treeDir\\tree.dot', '-o', 'tree' + str(i) + '.png',
+          '-Gdpi=600'], cwd='C:\\Users\\eilee\\Desktop\\pythonProject\\treeDir\\', shell=True)
+"""
 """
