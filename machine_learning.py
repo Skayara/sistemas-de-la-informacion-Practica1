d3e8c6cc71ef1bd8133791ebcc606c01ce7b8d88
Lu-all <lucianaallende60@outlook.com>
2022-05-03 19:10:06 +0200

    Machine Learning Fixed

--- machine_learning.py
+++ machine_learning.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 from sklearn.tree import export_graphviz
 import numpy as np
 
@@ -22,40 +22,42 @@ Load Data
 user_JSON = open(os.getcwd()+"/resources/json/users_IA_clases.json".replace('/', '\\'), "r")
 user_info = json.load(user_JSON)
 
-usuario = []
-emails_phishing_recibidos = []
-emails_phishing_clicados = []
-emails = []
+
+emails_click = []
+phishing_recibidos = []
 vulnerable = []
+prob_click = []
 
 for line in user_info['usuarios']:
-    usuario.append(line['usuario'].replace(".", " "))
-    emails_phishing_recibidos.append(line['emails_phishing_recibidos'])
-    emails_phishing_clicados.append(line['emails_phishing_clicados'])
-    if line['emails_phishing_recibidos'] != 0:
-        emails.append(line['emails_phishing_clicados'] / line['emails_phishing_recibidos'])
-    else:
-        emails.append(0)
-
+    phishing_recibidos.append(line['emails_phishing_recibidos'])
+    emails_click.append(line['emails_phishing_clicados'])
     vulnerable.append(line['vulnerable'])
+    if (line['emails_phishing_recibidos'] != 0):
+        prob_click.append(line['emails_phishing_clicados']/line['emails_phishing_recibidos'])
+    else:
+        prob_click.append(0)
 
-user_X = pd.DataFrame({'phishing': emails})
+user_X = pd.DataFrame({'phishing_recibidos': phishing_recibidos, 'emails_click': emails_click})
 user_Y = pd.DataFrame({'vulnerable': vulnerable})
-
 """
 Regresion lineal
 """
 
-user_X_train = user_X[:-20]
-user_X_test = user_X[-20:]
+user_X_train = user_X[:20]
+usuario_X_test = user_X[20:]
+user_X_test = prob_click[20:]
+user_X_train = user_X_train.to_numpy().tolist()
+usuario_X_test = usuario_X_test.to_numpy().tolist()
 
-user_Y_train = user_Y[:-20]
-user_Y_test = user_Y[-20:]
+user_Y_train = user_Y[:20]
+user_Y_test = user_Y[20:]
+
+user_Y_train = user_Y_train.to_numpy().tolist()
 
 reg = LinearRegression()
 reg.fit(user_X_train, user_Y_train)
 print(reg.coef_)
-user_Y_pred = reg.predict(user_X_test)
+user_Y_pred = reg.predict(usuario_X_test)
 user_pred = []
 
 for valor in user_Y_pred:
@@ -64,18 +66,17 @@ for valor in user_Y_pred:
     else:
         user_pred.append(1)
 
-#print("Mean squared error: %.2f" % accuran (user_Y_test, user_pred))
+print("Accuracy: %.2f" % accuracy_score (user_Y_test, user_pred))
 # The coefficient of determination: 1 is perfect prediction
-print("Coefficient of determination: %.2f" % r2_score(user_Y_test, user_pred))
+print("Coefficient of determination: %.2f" % r2_score(user_Y_test, user_Y_pred))
 
 
 # Plot outputs
 
-plt.scatter(user_X_test.values.ravel(), user_Y_test, color="black")
-print("X", user_Y_test)
-print("score", r2_score(user_Y_test, user_pred))
+plt.scatter(user_X_test, user_Y_test, color="black")
+print("score", r2_score(user_Y_test, user_Y_pred))
 print("intercept", reg.intercept_)
-plt.plot(reg.coef_[0]*np.array(user_X_test.values.ravel())+reg.intercept_, user_pred, color="blue", linewidth=3)
+plt.plot(0.02*np.array(user_X_test)+reg.intercept_, user_X_test, color="blue", linewidth=3)
 plt.xticks()
 plt.yticks()
 plt.xlabel("Prob_click")
@@ -94,7 +95,7 @@ dot_data = tree.export_graphviz(clf)
 graph = graphviz.Source(dot_data)
 graph.render("machine_learning/tree_graph_render")
 dot_data = tree.export_graphviz(clf,
-                                feature_names=['email click'],
+                                feature_names=['email recibido', 'email click'],
                                 class_names=['No vulnerable', 'Vulnerable'],
                                 filled=True, rounded=True,
                                 special_characters=True)
@@ -106,14 +107,14 @@ Random forest
 """
 
 clf = RandomForestClassifier(max_depth=5, random_state=0, n_estimators=10)
-clf.fit(user_X_train.values.ravel().reshape(-1, 1), user_Y_train.values.ravel())
+clf.fit(user_X_train, user_Y_train)
 
 for i in range(len(clf.estimators_)):
     print(i)
     estimator = clf.estimators_[i]
     export_graphviz(estimator,
                     out_file='machine_learning/random_forest/random_forest.dot',
-                    feature_names=['email click'],
+                    feature_names=['email recibido', 'email click'],
                     class_names=['No vulnerable', 'Vulnerable'],
                     rounded=True, proportion=False,
                     precision=2, filled=True)
